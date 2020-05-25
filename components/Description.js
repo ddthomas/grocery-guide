c23534732fba04a3a318c95b652334393c44987a
David Thomas <davidthomas@Davids-MacBook-Air.local>
2020-05-25 18:32:23 -0500

    added screen and components

---
+++ components/Description.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+
+function Description ({ navigation }) {
+	return (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "left", padding: 15 }}>
+			<Text>
+            Bacon ipsum dolor amet elit burgdoggen irure adipisicing tempor, aute cupidatat short loin short ribs meatball deserunt venison enim doner ribeye. Id corned beef ball tip short loin jowl t-bone. Anim ribeye veniam fatback short ribs andouille ullamco pork belly. Corned beef est duis tail beef ribs. Mollit quis culpa, incididunt picanha anim filet mignon short ribs pastrami porchetta meatloaf meatball tri-tip id drumstick. Ex tongue sint boudin, drumstick prosciutto do fatback pig. Nostrud frankfurter aliqua, leberkas corned beef kevin est strip steak lorem boudin shankle.
+
+            Qui turducken turkey pariatur bresaola landjaeger, filet mignon culpa labore. Beef commodo sunt tenderloin filet mignon dolore meatloaf. Ipsum sausage laborum t-bone turkey pancetta. Adipisicing aliqua drumstick irure commodo, ground round in tempor elit dolor anim fugiat eiusmod.
+            </Text>
+		</View>
+	);
+}
+
+export default Description;
---
+++ components/Video.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+
+
+function Video ({ navigation }) {
+	return (
+		<View style={{ flex: 1, }}>
+			<MaterialCommunityIcons name="video-outline" size={128} color="black" />
+		</View>
+	);
+}
+
+export default Video;
