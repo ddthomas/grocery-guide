bab995061165d48e83706af19d612e47a2d30a87
Nate Smith <nate@falconlaboratories.io>
2020-05-26 12:58:40 -0500

    updating the .gitignore file

--- components/Video.js
+++ components/Video.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-
-function Video ({ navigation }) {
+function Video({ navigation }) {
 	return (
-		<View style={{ flex: 1, }}>
+		<View style={{ flex: 1 }}>
 			<MaterialCommunityIcons name="video-outline" size={128} color="black" />
 		</View>
 	);
