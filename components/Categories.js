bb37c526c97341a7eeb271026bbde599565b37ce
David Thomas <davidthomas@Davids-MacBook-Air.local>
2020-06-03 09:07:25 -0500

    added categories comonent

---
+++ components/Categories.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+
+function Category({ _id, category }) {
+	const navigation = useNavigation();
+	return <Button title={category} onPress={() => navigation.navigate("Category")} />;
+}
+
+export default function Categories({ data }) {
+	const categories = data;
+	return categories.map((category) => {
+		return <Category key={category._id} {...category} />;
+	});
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import Questions from "../components/Questions";
-
+import Categories from "../components/Categories";
 import API from "../libs/api";
+
 
 const api = API({});
 
 function HomeScreen({ navigation }) {
 	const questions = api.get({ key: "eduContent" });
+	const categories = api.get({ key: "eduContent" });
 
 	return (
 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
 			<Text>Grocery Aisles (Categories):</Text>
-			<Button title="Category" onPress={() => navigation.navigate("Category")} />
+			
+			<Categories data={categories} />
 
 			<Text>Top Questions:</Text>
 
