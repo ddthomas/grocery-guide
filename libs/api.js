18ccccea1cf63698c8b19a1e0d9b269136471729
David Thomas <ddthomas@users.noreply.github.com>
2020-06-22 16:58:30 -0500

    Revert "added dynamic content for eduContent text"

--- libs/api.js
+++ libs/api.js
@@ -1,18 +1,13 @@
 import eduContent from "../data/eduContent.json";
-import Questions from "../data/questions.json";
 
 const DATA = {
-	eduContent: eduContent,
-	Questions: Questions
+	eduContent: eduContent
 };
 
 module.exports = (options) => {
 	return {
 		get({ key }) {
 			return DATA[key];
-		},
-		getByID({ key, id }) {
-			return DATA[key].find(item => {return item._id === id})
 		},
 	};
 };
