ea66eccbec8eb77a026c5eedbbeb469c8db5b74d
David Thomas <davidthomas@Davids-MacBook-Air.local>
2020-07-07 22:28:11 -0500

    add Questions component to CategoryScreen

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -11,27 +11,43 @@ function CategoryScreen({ navigation, route }) {
 	const { categoryID } = route.params;
 	const eduContent = api.getByCategoryID({ key: "eduContent", id: categoryID });
 	console.log(eduContent);
+	const category = api.getByID({ key: "categories", id: categoryID });
 
 	function renderEduContent(eduContent) {
-		return eduContent.map((item) => {
-			const category = api.getByID({
-				key: "categories",
-				id: item.categoryID,
-			});
-			return (
-				<View key={item._id}>
-					<Text>{item.question}</Text>
-					<Text>
-						{item.class} > {category.categoryName}
-					</Text>
-				</View>
-			);
-		});
+		return (
+			<View>
+				<Text>{category.categoryName}</Text>
+				<Questions data={eduContent} />
+			</View>
+			
+		);
 	}
-
 	return (
 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>{renderEduContent(eduContent)}</View>
 	);
-}
+	}
+
+
+
+// 	function renderEduContent(eduContent) {
+// 		return eduContent.map((item) => {
+// 			const category = api.getByID({
+// 				key: "categories",
+// 				id: item.categoryID,
+// 			});
+// 			return (
+// 				<View key={item._id}>
+// 					<Text>{category.categoryName}</Text>
+// 					<Text>{item.question}</Text>
+										
+// 				</View>
+// 			);
+// 		});
+// 	}
+
+// 	return (
+// 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>{renderEduContent(eduContent)}</View>
+// 	);
+// }
 
 export default CategoryScreen;
